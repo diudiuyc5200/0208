43f30e17b12efc9595b180818f73e2787b3b1769
Tyler Nijmeh <tylernij@gmail.com>
2021-07-19 11:51:48 +0700

    sched: schedutil: Individualize rate limits
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    [fiqri19102002: Adapt for msm-4.14]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -22,6 +22,8 @@
 #include <linux/sched/rt.h>
 
 #define SUGOV_KTHREAD_PRIORITY	50
+#define UP_RATE_LIMIT_US		(1000)
+#define DOWN_RATE_LIMIT_US		(1000)
 
 /* 添加 task_is_booster 函数声明 */
 extern bool task_is_booster(struct task_struct *p);
@@ -1095,10 +1097,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
+	tunables->up_rate_limit_us = UP_RATE_LIMIT_US;
+	tunables->down_rate_limit_us = DOWN_RATE_LIMIT_US;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
