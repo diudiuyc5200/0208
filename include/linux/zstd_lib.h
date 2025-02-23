a7e03b2e2e201fe5be0cee50df2fb3401bf93da5
Pzqqt <821026875@qq.com>
2025-02-23 13:40:27 +0800

    lib: zstd: Upgrade to latest upstream zstd version v1.5.7
    Follow the steps: https://github.com/facebook/zstd/blob/v1.5.7/contrib/linux-kernel/README.md
    
    The following committed changes are also preserved:
    70168143b302378ccff178bf28cb140c783dac88 zstd: merge optimization from Samsung <DevriesL>
    0b893588096921a258f8ca27d6005da4201c9b58 lib: zstd: Don't inline functions in zstd_opt.c <Nick Terrell>

--- include/linux/zstd_lib.h
+++ include/linux/zstd_lib.h
@@ -1489,7 +1489,7 @@ typedef struct {
  * @return : 0 => header is complete, `zfhPtr` is correctly filled,
  *          >0 => `srcSize` is too small, @return value is the wanted `srcSize` amount, `zfhPtr` is not filled,
  *           or an error code, which can be tested using ZSTD_isError() */
-ZSTDLIB_STATIC_API size_t ZSTD_getFrameHeader(ZSTD_frameHeader* zfhPtr, const void* src, size_t srcSize);   /**< doesn't consume input */
+ZSTDLIB_STATIC_API size_t ZSTD_getFrameHeader(ZSTD_FrameHeader* zfhPtr, const void* src, size_t srcSize);
 /*! ZSTD_getFrameHeader_advanced() :
  *  same as ZSTD_getFrameHeader(),
  *  with added capability to select a format (like ZSTD_f_zstd1_magicless) */
@@ -2286,8 +2286,7 @@ ZSTDLIB_STATIC_API size_t ZSTD_CCtx_refPrefix_advanced(ZSTD_CCtx* cctx, const vo
  * This parameter can be used to set an upper bound on the blocksize
  * that overrides the default ZSTD_BLOCKSIZE_MAX. It cannot be used to set upper
  * bounds greater than ZSTD_BLOCKSIZE_MAX or bounds lower than 1KB (will make
- * compressBound() innacurate). Only currently meant to be used for testing.
- *
+ * compressBound() inaccurate). Only currently meant to be used for testing.
  */
 #define ZSTD_c_maxBlockSize ZSTD_c_experimentalParam18
 
